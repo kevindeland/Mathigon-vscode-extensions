84e8d88ec64fefc3444b60df5557e65fa3c83f36
Kevin DeLand <kevin.deland@gmail.com>
2020-08-13 16:51:46 -0400

    can find the id in content.md and jump to it in functions.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
 
 import * as vscode from 'vscode';
-import { resolveCliPathFromVSCodeExecutablePath } from 'vscode-test';
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -32,11 +31,76 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	});
 
+
+	/**
+	 * Given a document and a line, give the id of the containing section.
+	 * @param doc content.md document
+	 * @param line current line.
+	 */
+	function findContentSectionId(doc: vscode.TextDocument, line: number): string {
+
+		const searchText = "> id: ";
+
+		let found = false;
+		while (!found && line > -1) {
+			const currentLine = doc.lineAt(line).text;
+			if (currentLine.includes(searchText)) {
+				return currentLine.substr(currentLine.indexOf(searchText)+searchText.length);
+			}
+			line--;
+		}
+		return '';
+	}
+
+	/**
+	 * Given a document for functions.ts, find the location of id and return its Range.
+	 *
+	 * @param doc functions.ts document
+	 * @param id section id.
+	 */
+	function findIdInFunctions(doc: vscode.TextDocument, id: string): vscode.Range {
+
+		// first convert from this-convention to thisConvention.
+		let toCamelCase = "";
+		// easier way: only find instances of '-' (but it's gonna iterate anyways)
+		let nextUpper = false;
+		for (let i=0; i < id.length; i++) {
+			if (id.charAt(i) === '-') {
+				nextUpper = true;
+			} else {
+				toCamelCase += nextUpper ? id.charAt(i).toUpperCase() : id.charAt(i);
+				nextUpper = false;
+			}
+		}
+
+		const searchText = "export function " + toCamelCase;
+
+		// iterate til you find the right line.
+		// TODO: need exception for when there's no function.
+		let lineNum = 0;
+		while (lineNum < doc.lineCount) {
+
+			const currentLine = doc.lineAt(lineNum).text;
+			if (currentLine.includes(searchText)) {
+
+				// TODO: should also handle "export async function" (or just function).
+				// TODO: beware of functions with same beginnings e.g. pythagoras and pythagorasProof
+				const beginLine = "export function ".length;;
+				const endLine = beginLine + toCamelCase.length;
+				return new vscode.Range(lineNum,beginLine,lineNum,endLine);
+			}
+			lineNum++;
+		}
+
+		throw Error('Not found');
+	}
+
+
 	let filename = vscode.commands.registerCommand('helloworld.filename', () => {
 		const editor = vscode.window.activeTextEditor;
 
 		const uri = editor?.document.uri;
-		const document = editor?.document;
+		const document = editor?.document!;
 		const fullFilePath = document?.fileName;
 		
 		let array = fullFilePath?.split('/')!;
@@ -44,14 +108,17 @@ export function activate(context: vscode.ExtensionContext) {
 
 		const filepath = array.join('/');
 
-		const currentLine = vscode.window.activeTextEditor?.selection.active.line;
+		const currentLine = vscode.window.activeTextEditor?.selection.active.line!;
+		const id = findContentSectionId(document, currentLine);
+		console.log(`found id: ${id}`);
+
 		vscode.window.showInformationMessage(`${currentLine}`);
 
 		if (filename?.toString() === 'content.md') {
 			vscode.window.showInformationMessage(`${filename}`);
 
 			vscode.workspace.openTextDocument(filepath.concat('/functions.ts')).then(doc => {
-				const options = {selection: new vscode.Range(3,0,5,0)};
+				const options = {selection: findIdInFunctions(doc, id)};
 				vscode.window.showTextDocument(doc, options);
 			});
 		} else if (filename?.toString() === 'functions.ts') {
