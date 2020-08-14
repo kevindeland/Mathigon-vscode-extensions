a6c767623a94535447b80dea381e9fbe41ba3a63
Kevin DeLand <kevin.deland@gmail.com>
2020-08-14 14:25:42 -0400

    Use DefinitionProvider to GoToDefinition of Markdown id sections

--- src/extension.ts
+++ src/extension.ts
@@ -9,7 +9,12 @@ export function activate(context: vscode.ExtensionContext) {
 
 	let disp2 = vscode.commands.registerCommand('helloworld.printVars', () => {
 
-		const vary = 'null';
+		console.log('printing vars');
+		let vary = 'null';
+
+		const editor = vscode.window.activeTextEditor;
+		const document = editor?.document;
+		vary = document?.languageId!;
 
 		vscode.workspace.openTextDocument();
 		vscode.window.showInformationMessage(`${vary}`);
@@ -52,13 +57,49 @@ export function activate(context: vscode.ExtensionContext) {
 		return '';
 	}
 
+	function findIdLocationInFunctions(doc: vscode.TextDocument, id: string): vscode.Location {
+
+		// first convert from this-convention to thisConvention.
+		let toCamelCase = "";
+		// easier way: only find instances of '-' (but it's gonna iterate anyways)
+		let nextUpper = false;
+		for (let i=0; i < id.length; i++) {
+			if (id.charAt(i) === '-') {
+				nextUpper = true;
+			} else {
+				toCamelCase += nextUpper ? id.charAt(i).toUpperCase() : id.charAt(i);
+				nextUpper = false;
+			}
+		}
+
+		const searchText = "export function " + toCamelCase;
+
+		// iterate til you find the right line.
+		// TODO: need exception for when there's no function.
+		let lineNum = 0;
+		while (lineNum < doc.lineCount) {
+
+			const currentLine = doc.lineAt(lineNum).text;
+			if (currentLine.includes(searchText)) {
+
+				// TODO: should also handle "export async function" (or just function).
+				// TODO: beware of functions with same beginnings e.g. pythagoras and pythagorasProof
+				const beginLine = "export function ".length;;
+				const endLine = beginLine + toCamelCase.length;
+				return new vscode.Location(doc.uri, new vscode.Range(lineNum, beginLine, lineNum, endLine));
+			}
+			lineNum++;
+		}
+		throw Error('Not found');
+	}
+
 	/**
 	 * Given a document for functions.ts, find the location of id and return its Range.
 	 *
 	 * @param doc functions.ts document
 	 * @param id section id.
 	 */
-	function findIdInFunctions(doc: vscode.TextDocument, id: string): vscode.Range {
+	function findIdRangeInFunctions(doc: vscode.TextDocument, id: string): vscode.Range {
 
 		// first convert from this-convention to thisConvention.
 		let toCamelCase = "";
@@ -118,7 +159,7 @@ export function activate(context: vscode.ExtensionContext) {
 			vscode.window.showInformationMessage(`${filename}`);
 
 			vscode.workspace.openTextDocument(filepath.concat('/functions.ts')).then(doc => {
-				const options = {selection: findIdInFunctions(doc, id)};
+				const options = {selection: findIdRangeInFunctions(doc, id)};
 				vscode.window.showTextDocument(doc, options);
 			});
 		} else if (filename?.toString() === 'functions.ts') {
@@ -134,6 +175,50 @@ export function activate(context: vscode.ExtensionContext) {
 
 	});
 
-	context.subscriptions.push(reverser, disposable, disp2, filename);
+	let defProvider = vscode.languages.registerDefinitionProvider('markdown', {
+
+		provideDefinition(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): vscode.ProviderResult<vscode.Location | vscode.Location[] | vscode.LocationLink[]> {
+
+			const fullFilePath = document?.fileName;
+
+			let array = fullFilePath?.split('/')!;
+			const filename = array.pop();
+
+			const filepath = array.join('/');
+
+			const currentLine = vscode.window.activeTextEditor?.selection.active.line!;
+
+			vscode.window.showInformationMessage(`${currentLine}`);
+
+			const lineText = document.lineAt(position.line).text;
+			const searchText = '> id: ';
+
+			console.log('Inside of the definition provider');
+
+			if (lineText.startsWith(searchText)) {
+				const id =  lineText.substr(lineText.indexOf(searchText)+searchText.length);
+				console.log(`found an id ${id}`);
+
+				let resultX;
+				return vscode.workspace.openTextDocument(filepath.concat('/functions.ts')).then(doc => {
+					console.log('opened new document');
+					resultX = findIdLocationInFunctions(doc, id);
+					console.log(`inside: ${resultX}`);
+					return resultX;
+				});
+
+				console.log(`outside: ${resultX}`);
+			}
+		}
+
+	});
+
+	vscode.languages.registerHoverProvider('typescript', {
+		provideHover(doc: vscode.TextDocument) {
+			return new vscode.Hover(`Mission Accomplished`);
+		}
+	})
+
+	context.subscriptions.push(reverser, disposable, disp2, filename, defProvider);
 }
 export function deactivate() {}
